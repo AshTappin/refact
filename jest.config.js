3a22e894ced521cf5edac1b14806fca1d0a49336
Ash <ash.tappin@blackpepper.co.uk>
2019-02-28 11:28:30 +0000

    Ensurte server starts with tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ],
+    moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    }
+};
